7122f52dab5af72bbee7fef77c5ba62db3f6451c
Neha Oudin <neha@oudin.red>
2024-11-29 11:21:49 +0100

    test: fix test charm

--- tests/unit/mongodb_test_charm/src/charm.py
+++ tests/unit/mongodb_test_charm/src/charm.py
@@ -15,7 +15,7 @@ from single_kernel_mongo.managers.mongodb_operator import MongoDBOperator
 class MongoTestCharm(AbstractMongoCharm[MongoDBCharmConfig, MongoDBOperator]):
     config_type = MongoDBCharmConfig
     operator_type = MongoDBOperator
-    substrate = Substrates.K8S
+    substrate = Substrates.VM
     peer_rel_name = RelationNames.PEERS.value
     name = "mongodb-test"
 
