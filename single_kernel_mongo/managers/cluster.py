ab97d285b6d4fe098b456edbbe8d62d100cfb175
Neha Oudin <neha@oudin.red>
2025-01-10 09:40:09 +0100

    test: fix invalid statuses

--- single_kernel_mongo/managers/cluster.py
+++ single_kernel_mongo/managers/cluster.py
@@ -262,7 +262,6 @@ class ClusterRequirer(Object):
             self.state.app_peer_data.db_initialised = True
 
         self.dependent.share_connection_info()
-        self.charm.status_manager.process_and_share_statuses()
 
     def remove_users_and_cleanup_mongo(self, relation: Relation) -> None:
         """Proceeds on relation broken."""
@@ -278,7 +277,6 @@ class ClusterRequirer(Object):
         logger.info("Cleaning database and user removed for mongos application")
         self.state.secrets.remove(Scope.APP, AppPeerDataKeys.USERNAME.value)
         self.state.secrets.remove(Scope.APP, AppPeerDataKeys.PASSWORD.value)
-        self.charm.status_manager.process_and_share_statuses()
 
     def update_users(self) -> None:
         """Updates users after being initialised."""
