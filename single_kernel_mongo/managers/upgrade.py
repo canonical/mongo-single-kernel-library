49c5e3ea08a8054da982fb9ec0564939f84bf92e
Neha Oudin <neha@oudin.red>
2025-03-03 10:00:24 +0100

    fix: all units should get new revision in databag

--- single_kernel_mongo/managers/upgrade.py
+++ single_kernel_mongo/managers/upgrade.py
@@ -88,12 +88,13 @@ class MongoUpgradeManager(Generic[T], GenericMongoDBUpgradeManager[T]):
             self.dependent.upgrade_events.post_app_upgrade_event.emit()
 
     def _on_vm_upgrade(self):
+        if not self.state.upgrade_in_progress:
+            self.state.unit_upgrade_peer_data.current_revision = (
+                self.dependent.cross_app_version_checker.version  # type: ignore
+            )
         if self.charm.unit.is_leader():
             if not self.state.upgrade_in_progress:
                 logger.info("Charm refreshed. MongoDB version unchanged")
-                self.state.unit_upgrade_peer_data.current_revision = (
-                    self.dependent.cross_app_version_checker.version  # type: ignore
-                )
             if self.dependent.name == CharmKind.MONGOD:
                 self.state.app_upgrade_peer_data.upgrade_resumed = False
                 self.dependent.cross_app_version_checker.set_version_across_all_relations()  # type: ignore
