4974953b7f452260afa48956941ae4fc125cd386
Neha Oudin <neha@oudin.red>
2025-03-12 09:47:42 +0100

    feat: move to cp lib instead of building wheel

--- scripts/build_lib_for_integration.sh
+++ scripts/build_lib_for_integration.sh
@@ -2,17 +2,9 @@
 
 ## This builds the whl and then copies it to all 4 test charms and updates the requirements file.
 
-DIST="dist"
-
-if [ -d "${DIST}" ]; then
-    rm -rf dist
-fi
-
 git_hash=$(git describe --always --dirty)
 
-poetry build --format=wheel --output=$DIST
-
-WHL_PATH=$(find dist -name "*.whl")
+LIB_PATH="./single_kernel_mongo"
 
 CHARMS_PATH="./tests/charms"
 
@@ -23,9 +15,14 @@ else
 fi
 
 for directory in "${TEST_CHARMS[@]}"; do
-    cp -r "$DIST" "$directory"
+    directory_lib_path="${directory}/${LIB_PATH}"
+    rm -rf "$directory_lib_path"
+    mkdir "$directory_lib_path"
+    cp -r "${LIB_PATH}" "$directory_lib_path"
+    cp "pyproject.toml" "$directory_lib_path"
+    cp "README.md" "$directory_lib_path"
     cp "${directory}/requirements.txt" "${directory}/requirements.txt.backup"
-    echo "mongo-charms-single-kernel @ file:/root/project/${WHL_PATH}" > "${directory}/requirements.txt"
+    echo "mongo-charms-single-kernel @ file:${LIB_PATH}" > "${directory}/requirements.txt"
     poetry export --without-hashes >> "${directory}/requirements.txt"
 
     echo "Building charm ${directory}\n"
@@ -34,6 +31,8 @@ for directory in "${TEST_CHARMS[@]}"; do
     pushd $directory
     python3 -c 'import pathlib; import shutil; import subprocess; git_hash=subprocess.run(["git", "describe", "--always", "--dirty"], capture_output=True, check=True, encoding="utf-8").stdout; file = pathlib.Path("charm_version"); shutil.copy(file, pathlib.Path("charm_version.backup")); version = file.read_text().strip(); file.write_text(f"{version}+{git_hash}")'
     charmcraft -v pack
+
+    rm ${LIB_PATH} -rf
     mv charm_version.backup charm_version
     popd
     mv "${directory}/requirements.txt.backup" "${directory}/requirements.txt"
