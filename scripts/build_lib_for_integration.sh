9862a2961d10c50b9a2d0380dad17b060ed9462e
Neha Oudin <17551419+Gu1nness@users.noreply.github.com>
2025-03-12 12:19:43 +0100

    Apply suggestions from code review
    Co-authored-by: Mia Altieri <32723809+MiaAltieri@users.noreply.github.com>

--- scripts/build_lib_for_integration.sh
+++ scripts/build_lib_for_integration.sh
@@ -15,9 +15,11 @@ else
 fi
 
 for directory in "${TEST_CHARMS[@]}"; do
+    echo "clearing out libs for charm"
     directory_lib_path="${directory}/${LIB_PATH}"
     rm -rf "$directory_lib_path"
     mkdir "$directory_lib_path"
+    echo "copying over libs from single kernel charm" 
     cp -r "${LIB_PATH}" "$directory_lib_path"
     cp "pyproject.toml" "$directory_lib_path"
     cp "README.md" "$directory_lib_path"
@@ -31,7 +33,7 @@ for directory in "${TEST_CHARMS[@]}"; do
     pushd $directory
     python3 -c 'import pathlib; import shutil; import subprocess; git_hash=subprocess.run(["git", "describe", "--always", "--dirty"], capture_output=True, check=True, encoding="utf-8").stdout; file = pathlib.Path("charm_version"); shutil.copy(file, pathlib.Path("charm_version.backup")); version = file.read_text().strip(); file.write_text(f"{version}+{git_hash}")'
     charmcraft -v pack
-
+     echo "removing copied files from single kernel charm."
     rm ${LIB_PATH} -rf
     mv charm_version.backup charm_version
     popd
