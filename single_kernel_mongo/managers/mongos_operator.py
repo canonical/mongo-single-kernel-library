ec8dc9c5629d3848561d973cdf272b93f41a4d76
Mia Altieri <mgaltier200@gmail.com>
2025-03-25 09:02:44 +0100

    fmt+lint

--- single_kernel_mongo/managers/mongos_operator.py
+++ single_kernel_mongo/managers/mongos_operator.py
@@ -81,9 +81,7 @@ class MongosOperator(OperatorProtocol, Object):
         )
 
         container = (
-            self.charm.unit.get_container(self.name)
-            if self.substrate == Substrates.K8S
-            else None
+            self.charm.unit.get_container(self.name) if self.substrate == Substrates.K8S else None
         )
 
         self.workload = get_mongos_workload_for_substrate(self.substrate)(
@@ -109,9 +107,7 @@ class MongosOperator(OperatorProtocol, Object):
         self.cluster_manager = ClusterRequirer(
             self, self.workload, self.state, self.substrate, RelationNames.CLUSTER
         )
-        upgrade_backend = (
-            MachineUpgrade if self.substrate == Substrates.VM else KubernetesUpgrade
-        )
+        upgrade_backend = MachineUpgrade if self.substrate == Substrates.VM else KubernetesUpgrade
         self.upgrade_manager = MongosUpgradeManager(
             self, upgrade_backend, key=RelationNames.UPGRADE_VERSION.value
         )
@@ -386,9 +382,7 @@ class MongosOperator(OperatorProtocol, Object):
             ).split(",")
         )
         external_connectivity = json.loads(
-            data_interface.fetch_relation_field(
-                relation.id, "external-node-connectivity"
-            )
+            data_interface.fetch_relation_field(relation.id, "external-node-connectivity")
             or "false"
         )
 
@@ -460,17 +454,11 @@ class MongosOperator(OperatorProtocol, Object):
 
         if self.substrate == Substrates.VM:
             if self.state.app_peer_data.external_connectivity:
-                host = (
-                    self.state.unit_peer_data.internal_address
-                    + f":{MongoPorts.MONGOS_PORT}"
-                )
+                host = self.state.unit_peer_data.internal_address + f":{MongoPorts.MONGOS_PORT}"
             else:
                 host = self.state.formatted_socket_path
         else:
-            host = (
-                self.state.unit_peer_data.internal_address
-                + f":{MongoPorts.MONGOS_PORT}"
-            )
+            host = self.state.unit_peer_data.internal_address + f":{MongoPorts.MONGOS_PORT}"
 
         uri = f"mongodb://{host}"
 
@@ -521,9 +509,7 @@ class MongosOperator(OperatorProtocol, Object):
                 self.charm.config["expose-external"],
                 "['nodeport', 'none']",
             )
-            charm_statuses.append(
-                CharmStatuses.mongos.value.INVALD_EXPOSE_EXTERNAL.value
-            )
+            charm_statuses.append(CharmStatuses.mongos.value.INVALD_EXPOSE_EXTERNAL.value)
 
         if not self.workload.workload_present:
             charm_statuses.append(CharmStatuses.MONGODB_NOT_INSTALLED.value)
@@ -542,25 +528,16 @@ class MongosOperator(OperatorProtocol, Object):
             charm_statuses.append(status)
             return charm_statuses
 
-        if (
-            self.state.mongos_cluster_relation
-            and not self.state.cluster.config_server_uri
-        ):
-            charm_statuses.append(
-                CharmStatuses.mongos.value.CONNECTING_TO_CONFIG_SERVER.value
-            )
+        if self.state.mongos_cluster_relation and not self.state.cluster.config_server_uri:
+            charm_statuses.append(CharmStatuses.mongos.value.CONNECTING_TO_CONFIG_SERVER.value)
 
         if not self.is_mongos_running():
             logger.info("mongos has not started yet")
             charm_statuses.append(CharmStatuses.MONGOS_NOT_STARTED.value)
             return charm_statuses
 
-        username = self.state.secrets.get_for_key(
-            Scope.APP, key=AppPeerDataKeys.USERNAME.value
-        )
-        password = self.state.secrets.get_for_key(
-            Scope.APP, key=AppPeerDataKeys.PASSWORD.value
-        )
+        username = self.state.secrets.get_for_key(Scope.APP, key=AppPeerDataKeys.USERNAME.value)
+        password = self.state.secrets.get_for_key(Scope.APP, key=AppPeerDataKeys.PASSWORD.value)
         if not username or not password:
             charm_statuses.append(CharmStatuses.mongos.value.WAITING_FOR_SECRETS.value)
 
