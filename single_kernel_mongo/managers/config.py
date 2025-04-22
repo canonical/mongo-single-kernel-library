b0038849c23f7ebb30ffea028b75de48281fbe5a
Neha Oudin <neha@oudin.red>
2025-04-22 14:00:46 +0200

    fix: bugfix userToDNMapping

--- single_kernel_mongo/managers/config.py
+++ single_kernel_mongo/managers/config.py
@@ -496,7 +496,7 @@ class MongosConfigManager(MongoConfigManager):
         if not self.state.ldap.is_ready():
             return {}
 
-        return {
+        ldap_params: dict[str, Any] = {
             "security": {
                 "ldap": {
                     "servers": ",".join(self.state.ldap.formatted_ldap_urls),
@@ -510,6 +510,11 @@ class MongosConfigManager(MongoConfigManager):
             },
             "setParameter": {"authenticationMechanisms": "PLAIN,SCRAM-SHA-256"},
         }
+        if self.state.ldap.ldap_user_to_dn_mapping:
+            ldap_params["security"]["ldap"]["userToDNMapping"] = (
+                self.state.ldap.ldap_user_to_dn_mapping
+            )
+        return ldap_params
 
     @override
     def build_config(self) -> dict[str, Any]:
