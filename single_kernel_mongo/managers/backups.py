2b95e9d9639e03c1ff2eb9767a7093bac3eaae18
Neha Oudin <neha@oudin.red>
2025-07-25 17:34:57 +0200

    fix: Wait before set config in case a resync is in progress

--- single_kernel_mongo/managers/backups.py
+++ single_kernel_mongo/managers/backups.py
@@ -565,6 +565,8 @@ class BackupManager(Object, BackupConfigManager, ManagerStatusProtocol):
             # Clear the current config file.
             self.clear_pbm_config_file()
 
+        self._wait_pbm_status()
+
         config = map_s3_config_to_pbm_config(credentials)
 
         try:
