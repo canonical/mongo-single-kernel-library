bb716ef1e74abf8360d0a4d3e41505b7b39c067a
Neha Oudin <neha@oudin.red>
2025-07-25 14:28:39 +0200

    fix: endpoint is mandatory

--- single_kernel_mongo/managers/backups.py
+++ single_kernel_mongo/managers/backups.py
@@ -513,7 +513,7 @@ class BackupManager(Object, BackupConfigManager, ManagerStatusProtocol):
             logger.info("Missing region - this is required for AWS and GCP")
 
         if not provided_configs.get("storage.s3.endpointUrl"):
-            logger.info("Missing endpointUrl - this is required for MinIO and GCP")
+            return False
 
         return True
 
