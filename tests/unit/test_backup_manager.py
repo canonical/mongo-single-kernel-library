60daf3ff991ca8e2436d3ce943ba157ab862a26d
Neha Oudin <neha@oudin.red>
2025-07-28 14:07:43 +0200

    fix: unit test

--- tests/unit/test_backup_manager.py
+++ tests/unit/test_backup_manager.py
@@ -102,8 +102,8 @@ def test_get_status_fail(harness: Harness[MongoTestCharm], backup_manager: Backu
     ("pbm_status", "expected"),
     (
         ("status code: 403", "s3 credentials are incorrect."),
-        ("status code: 404", "s3 configurations are incompatible."),
-        ("status code: 301", "s3 configurations are incompatible."),
+        ("status code: 404", "s3 config options are incompatible."),
+        ("status code: 301", "s3 config options are incompatible."),
         ("Unknown message", "Unknown PBM error, check logs."),
         (
             '{"cluster": [{"nodes":[{"host": "mongodb/10.0.0.10:27017", "errors": "status code: 403"}], "rs": "mongodb"}]}',
