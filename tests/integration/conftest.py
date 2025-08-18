fbcedf3e8c71a22a4bbe0d8e681893ff55819295
Neha Oudin <17551419+Gu1nness@users.noreply.github.com>
2025-08-18 13:02:50 +0200

    patch(DPE-7399): Migrate unit tests to single kernel library (#60)
    * feat(DPE-7399): Migrate unit tests to single kernel library
    
    * fix: Add test for revision checker integration

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -10,7 +10,6 @@ from pathlib import Path
 from typing import Any
 
 import pytest
-from _pytest.config.argparsing import Parser
 from pytest_operator.plugin import OpsTest
 from yaml import safe_load
 
@@ -42,37 +41,6 @@ from .helpers.types import Substrate
 logger = getLogger(__name__)
 
 
-def pytest_addoption(parser: Parser):
-    parser.addoption(
-        "--substrate",
-        action="store",
-        help="Substrate to test, either lxd or microk8s",
-        choices=("lxd", "microk8s"),
-    )
-
-
-def pytest_configure(config):
-    config.addinivalue_line(
-        "markers", "skip_if_substrate(substrate): skip test for the given substrate"
-    )
-
-
-@pytest.fixture(scope="session")
-def substrate(request) -> Substrate:
-    """The substrate that we are testing."""
-    return request.config.option.substrate
-
-
-# To skip tests
-
-
-@pytest.fixture(autouse=True)
-def skip_for_substrate(request, substrate: Substrate):
-    if mark := request.node.get_closest_marker("skip_if_substrate"):
-        if mark.args[0] == substrate:
-            pytest.skip(f"This test does not run on {substrate}")
-
-
 @pytest.fixture
 def application_path() -> str:
     """The test application path."""
@@ -89,22 +57,6 @@ def client_relation_charm_path() -> str:
 def mongos_client_application_path() -> str:
     """The mongos test application path."""
     return "./tests/integration/applications/mongos_client_charm/test-routing-application_ubuntu@22.04-amd64.charm"
-
-
-@pytest.fixture
-def mongod_base_path(substrate) -> Path:
-    """The base path for the files of the mongodb charms, according to the substrate."""
-    if substrate == "microk8s":
-        return Path("tests/charms/mongodb_k8s_test_charm")
-    return Path("tests/charms/mongodb_test_charm")
-
-
-@pytest.fixture
-def mongos_base_path(substrate) -> Path:
-    """The base path for the files of the mongos charms, according to the substrate."""
-    if substrate == "microk8s":
-        return Path("tests/charms/mongos_k8s_test_charm")
-    return Path("tests/charms/mongos_test_charm")
 
 
 @pytest.fixture
