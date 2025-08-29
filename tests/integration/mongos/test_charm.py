b781e0cf850d823a18bf4509280659a1e96be92f
Patricia Reinoso <patricia.reinoso@canonical.com>
2025-08-29 09:32:53 +0200

    integration test

--- tests/integration/mongos/test_charm.py
+++ tests/integration/mongos/test_charm.py
@@ -8,7 +8,6 @@ from pytest_operator.plugin import OpsTest
 from ..helpers.common import (
     MONGOS_APP_NAME,
     MONGOS_PORT,
-    check_status_detail,
     execute_on_mongod,
     get_mongodb_hostname_for_unit,
     remove_units,
@@ -69,12 +68,6 @@ async def test_waits_for_config_server(ops_test: OpsTest, substrate: Substrate) 
         status="Missing relation to config-server.",
         timeout=300,
         subordinate=(substrate == "lxd"),
-    )
-    await check_status_detail(
-        ops_test,
-        MONGOS_APP_NAME,
-        status="blocked",
-        message="Missing relation to config-server.",
     )
 
 
