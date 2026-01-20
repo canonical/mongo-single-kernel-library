33ef3e0dc5fe8103ba73afa09b47b5258d9e3cbb
Neha Oudin <neha@oudin.red>
2026-01-20 09:29:38 +0100

    patch: Set IPv4 Read Timeout

--- single_kernel_mongo/config/literals.py
+++ single_kernel_mongo/config/literals.py
@@ -102,7 +102,11 @@ PBM_RESTART_DELAY = 5
 FEATURE_VERSION = "8.0"
 
 
-OS_REQUIREMENTS = {"vm.max_map_count": "262144", "vm.overcommit_memory": "1"}
+OS_REQUIREMENTS = {
+    "vm.max_map_count": "262144",
+    "vm.overcommit_memory": "1",
+    "net.ipv4.tcp_keepalive_time": "120",
+}
 
 TRUST_STORE_PATH = Path("/usr/local/share/ca-certificates")
 
