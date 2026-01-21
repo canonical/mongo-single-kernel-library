4f81e9c0732c228ab3c7157fe7f65885ed766a55
Patricia Reinoso <patricia.reinoso@canonical.com>
2026-01-21 15:02:11 +0100

    patch: bump snap

--- single_kernel_mongo/config/literals.py
+++ single_kernel_mongo/config/literals.py
@@ -68,10 +68,10 @@ class Snap:
 
     name: str = "charmed-mongodb"
     channel: str = "6/edge"
-    revision: str = "139"
+    revision: str = "147"
 
 
-SNAP = Snap(channel="6/edge", revision="139")
+SNAP = Snap(channel="6/edge", revision="147")
 
 T = TypeVar("T", bound=str | int)
 
