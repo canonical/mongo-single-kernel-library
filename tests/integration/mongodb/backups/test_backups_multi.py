bf8002b88be06319352fa78e08117aaaf81f8009
Neha Oudin <neha@oudin.red>
2026-01-26 14:10:04 +0100

    fix: gcs integrator

--- tests/integration/mongodb/backups/test_backups_multi.py
+++ tests/integration/mongodb/backups/test_backups_multi.py
@@ -56,7 +56,7 @@ async def test_deploy_charms(
     )
     # deploy the s3 integrator charm
     await ops_test.model.deploy(S3_APP_NAME, channel="edge")
-    await ops_test.model.deploy(GCS_APP_NAME, channel="edge")
+    await ops_test.model.deploy(GCS_APP_NAME, channel="1/edge")
 
     await ops_test.model.wait_for_idle(timeout=DEPLOYMENT_TIMEOUT)
 
