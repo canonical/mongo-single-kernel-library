0a579e1dedc1a5fa4fead5d019dad9edd340734c
Neha Oudin <neha@oudin.red>
2026-01-28 17:59:09 +0100

    fix: wrong error…

--- single_kernel_mongo/events/backups.py
+++ single_kernel_mongo/events/backups.py
@@ -262,7 +262,7 @@ class BackupEventsHandler(Object):
         """Proceed on s3 relation broken."""
         try:
             manager = self.manager_for(event.relation)
-        except InvalidStorageCredentialsError:
+        except InvalidStorageRelationError:
             logger.warning("Two relations combined, exiting early.")
             return
         manager.cleanup_certs_and_restart(event.relation)
