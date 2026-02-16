e811f738560ea5a2b27db49a2a76a769146a4816
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2026-02-16 13:00:35 +0545

    Update tests to pass sharding test

--- single_kernel_mongo/managers/sharding.py
+++ single_kernel_mongo/managers/sharding.py
@@ -113,7 +113,7 @@ class ConfigServerManager(Object, ManagerStatusProtocol):
         }
 
         if self.state.s3_relation:
-            credentials = self.dependent.backup_events.s3_client.get_s3_connection_info()
+            credentials = self.dependent.backup_events.s3_client.get_storage_connection_info()
             if cert_chain_list := credentials.get("tls-ca-chain", None):
                 relation_data[AppShardingComponentKeys.BACKUP_CA_SECRET.value] = json.dumps(
                     cert_chain_list
