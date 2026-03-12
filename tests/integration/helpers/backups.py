3c2c525d4780a559728604930770737e416527a4
Neha Oudin <17551419+Gu1nness@users.noreply.github.com>
2026-03-12 16:36:00 +0000

    patch(DPE-9525): Shard integration and backups stability (#246)

--- tests/integration/helpers/backups.py
+++ tests/integration/helpers/backups.py
@@ -73,7 +73,8 @@ async def count_logical_backups(db_unit: JujuUnit) -> int:
     list_result = list_result.split("\n")
     backups = 0
     for res in list_result:
-        backups += 1 if "logical" in res else 0
+        if "logical" in res and "in progress" not in res:
+            backups += 1
 
     return backups
 
