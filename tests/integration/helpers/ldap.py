a96b5fdaeab473ee760fec49f1ca30258f7af5a1
Neha Oudin <neha@oudin.red>
2026-03-20 11:48:30 +0100

    fix: relation

--- tests/integration/helpers/ldap.py
+++ tests/integration/helpers/ldap.py
@@ -86,8 +86,9 @@ async def deploy_glauth(ops_test: OpsTest, kubernetes_model: Model) -> None:
 
         await kubernetes_model.wait_for_idle([TRAEFIK_CHARM], status="active")
 
-        await kubernetes_model.integrate(LDAP_APP_NAME, POSTGRESQL_K8S)
-        await kubernetes_model.integrate(LDAP_APP_NAME, CERTIFICATES)
+        await kubernetes_model.integrate(
+            f"{LDAP_APP_NAME}:pg-database", f"{POSTGRESQL_K8S}:database"
+        )
         await kubernetes_model.integrate(LDAP_APP_NAME, LDAP_UTILS_APP_NAME)
 
         await kubernetes_model.wait_for_idle(
