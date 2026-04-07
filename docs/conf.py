d1dd2405248dda157c10925101c0814f91eda49a
Andreia <andreia.velasco@canonical.com>
2026-04-07 13:47:31 +0200

    patch: update documentation starter pack version (8) (#279)
    <!-- Provide a general summary of your changes in the Title above -->
    <!-- markdownlint-disable MD041 -->
    
    ## 🏷️ Type of changes
    <!--- What types of changes does your code introduce? -->
    <!--- Put an `x` in all the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [x] Documentation update
    - [x] Tooling and CI
    - [ ] Dependencies upgrade or change
    - [ ] Chores / refactoring
    
    ## 📝 Description
    <!--
    Describe your changes in detail including:
      - the purpose and feature scope of this PR (what)
      - the motivation behind this change (why)
      - the impacted components (often matches conventional commit scope)
      - explanation/algorithm if required (how)
    -->
    
    This PR updates the [sphinx starter
    pack](https://github.com/canonical/sphinx-docs-starter-pack) files to
    the latest version, which includes new features and stability fixes.
    
    One of the new features is a cookie banner, which enables Google
    Analytics tracking
    ([DPE-9651](https://warthogs.atlassian.net/browse/DPE-9651)).
    
    <img width="1141" height="486" alt="image"
    src="https://github.com/user-attachments/assets/6bd80b00-400e-48d4-b5c7-89c808d0d2c9"
    />
    
    There are no breaking changes or changes that otherwise affect the
    MongoDB docs.
    
    Detailed starter pack release notes can be found
    [here](https://github.com/canonical/sphinx-docs-starter-pack/releases).
    
    ## 🧪 Manual testing steps
    <!--- Give a list of instructions to manually test your change. -->
    <!--- These instructions are meant for reviewers to test the PR -->
    <!--- on their development environment. -->
    
    ```shell
    cd docs
    make clean
    make install
    make run
    ```
    
    Make sure `make install` and `make run` do not output any errors or
    warnings.
    
    Open webserver at http://127.0.0.1:8000 in incognito browser, check
    cookie banner.
    
    ## 🔬 Automated testing steps
    <!--- Describe the unit and integration tests that you added -->
    <!--- to ensure that this feature works as expected. Include details -->
    <!--- on the positive and negative test cases, as well as any changes
    -->
    <!--- made to the existing tests to ensure they are still relevant. -->
    
    ## ✅ Checklist
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My code follows the code style of this project.
    - [x] I have added or updated any relevant documentation.
    - [x] I have read the [**CONTRIBUTING**](../blob/8/edge/CONTRIBUTING.md)
    document.
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.
    
    
    [DPE-9651]:
    https://warthogs.atlassian.net/browse/DPE-9651?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    
    ---------
    
    Signed-off-by: Andreia <andreia.velasco@canonical.com>

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@ import yaml
 # to keep all documentation based on it consistent and on brand:
 # https://github.com/canonical/canonical-sphinx
 
+
 #######################
 # Project information #
 #######################
@@ -26,6 +27,14 @@ import yaml
 project = "Charmed MongoDB"
 author = "Canonical Ltd."
 
+# The year in the copyright statement defaults to the current year, so
+# individual document versions show when they were built.
+# TODO: If the date must be a range, like in a software license, replace 
+# 2026 with the starting year of development and use:
+#
+# copyright = f"2026-{datetime.date.today().year}"
+
+copyright = f"{datetime.date.today().year}"
 
 # Sidebar documentation title; best kept reasonably short
 #
@@ -34,32 +43,6 @@ author = "Canonical Ltd."
 # TODO: To disable the title, set to an empty string.
 
 html_title = project + " 8" + " documentation"
-
-
-# Copyright string; shown at the bottom of the page
-#
-# Now, the starter pack uses CC-BY-SA as the license
-# and the current year as the copyright year.
-#
-# TODO: If your docs need another license, specify it instead of 'CC-BY-SA'.
-#
-# TODO: If your documentation is a part of the code repository of your project,
-#       it inherits the code license instead; specify it instead of 'CC-BY-SA'.
-#
-# NOTE: For static works, it is common to provide the first publication year.
-#       Another option is to provide both the first year of publication
-#       and the current year, especially for docs that frequently change,
-#       e.g. 2022–2023 (note the en-dash).
-#
-#       A way to check a repo's creation date is to get a classic GitHub token
-#       with 'repo' permissions; see https://github.com/settings/tokens
-#       Next, use 'curl' and 'jq' to extract the date from the API's output:
-#
-#       curl -H 'Authorization: token <TOKEN>' \
-#         -H 'Accept: application/vnd.github.v3.raw' \
-#         https://api.github.com/repos/canonical/<REPO> | jq '.created_at'
-
-copyright = "%s CC-BY-SA, %s" % (datetime.date.today().year, author)
 
 
 # Documentation website URL
@@ -131,11 +114,11 @@ html_context = {
     #
     # NOTE: If set, links for viewing the documentation source files
     #       and creating GitHub issues are added at the bottom of each page.
-    "github_url": "https://github.com/canonical/mongo-single-kernel-library",
+    "github_url": "ttps://github.com/canonical/mongo-single-kernel-library",
     # Docs branch in the repo; used in links for viewing the source files
     #
     # TODO: To customise the branch, uncomment and update as needed.
-    'repo_default_branch': '6/edge',
+    'repo_default_branch': '8/edge',
     # Docs location in the repo; used in links for viewing the source files
     #
 
@@ -150,7 +133,34 @@ html_context = {
 
     # Required for feedback button    
     'github_issues': 'enabled',
+
+    # Inherit the author value
+    "author": author,
+
+    # The starter pack uses CC-BY-SA as the license
+    #
+    # TODO: If your docs need another license, specify it instead of 'CC-BY-SA'.
+    # For the name, we recommend using the standard shorthand identifier from
+    # https://spdx.org/licenses
+    #
+    # For the URL, link directly to the license statement, typically found on
+    # the product's home page or in its GitHub project.
+    #
+    # TODO: If your documentation is a part of the code repository of your project,
+    #       it inherits the code license instead; specify it instead of 'CC-BY-SA'.
+
+    "license": {
+        "name": "CC-BY-SA-3.0",
+        "url": "https://github.com/canonical/mongo-single-kernel-library/blob/8/edge/LICENSE",
+    },
 }
+
+# html_extra_path = []
+
+# Allow opt-in build of the OpenAPI "Hello" example so docs stay clean by default.
+# if os.getenv("OPENAPI", ""):
+#     tags.add("openapi")
+#     html_extra_path.append("how-to/assets/openapi.yaml")
 
 # TODO: To enable the edit button on pages, uncomment and change the link to a
 # public repository on GitHub or Launchpad. Any of the following link domains
@@ -159,9 +169,9 @@ html_context = {
 # - https://launchpad.net/example
 # - https://git.launchpad.net/example
 #
-# html_theme_options = {
-# 'source_edit_link': 'https://github.com/canonical/sphinx-docs-starter-pack',
-# }
+html_theme_options = {
+'source_edit_link': 'https://github.com/canonical/mongo-single-kernel-library',
+}
 
 # Project slug; see https://meta.discourse.org/t/what-is-category-slug/87897
 #
@@ -174,18 +184,13 @@ html_context = {
 # Sitemap configuration: https://sphinx-sitemap.readthedocs.io/
 #######################
 
-# Base URL of RTD hosted project
+# Use RTD canonical URL to ensure duplicate pages have a specific canonical URL
 
-html_baseurl = "https://canonical-charmed-mongodb.readthedocs-hosted.com/"
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
 
-# URL scheme. Add language and version scheme elements.
-# When configured with RTD variables, check for RTD environment so manual runs succeed:
+# sphinx-sitemap uses html_baseurl to generate the full URL for each page:
 
-if 'READTHEDOCS_VERSION' in os.environ:
-    version = os.environ["READTHEDOCS_VERSION"]
-    sitemap_url_scheme = '{version}{link}'
-else:
-    sitemap_url_scheme = 'MANUAL/{link}'
+sitemap_url_scheme = '{link}'
 
 # Include `lastmod` dates in the sitemap:
 
@@ -202,26 +207,28 @@ sitemap_excludes = [
 # TODO: Add more pages to sitemap_excludes if needed. Wildcards are supported.
 #       For example, to exclude module pages generated by autodoc, add '_modules/*'.
 
-#######################
-# Template and asset locations
-#######################
+################################
+# Template and asset locations #
+################################
 
-#html_static_path = ["_static"]
-#templates_path = ["_templates"]
+# html_static_path = ["_static"]
+templates_path = ["_templates"]
 
 
 #############
 # Redirects #
 #############
 
-# To set up redirects: https://documatt.gitlab.io/sphinx-reredirects/usage.html
-# For example: 'explanation/old-name.html': '../how-to/prettify.html',
+# Add redirects to the 'redirects.txt' file
+# https://sphinxext-rediraffe.readthedocs.io/en/latest/
 
 # To set up redirects in the Read the Docs project dashboard:
 # https://docs.readthedocs.io/en/stable/guides/redirects.html
 
-# NOTE: If undefined, set to None, or empty,
-#       the sphinx_reredirects extension will be disabled.
+rediraffe_redirects = "redirects.txt"
+
+# Strips '/index.html' from destination URLs when building with 'dirhtml'
+rediraffe_dir_only = True
 
 
 ###########################
@@ -246,7 +253,7 @@ linkcheck_ignore = [
 linkcheck_anchors_ignore_for_url = [r"https://github\.com/.*"]
 
 # give linkcheck multiple tries on failure
-# linkcheck_timeout = 30
+linkcheck_timeout = 90
 linkcheck_retries = 3
 
 ########################
@@ -266,42 +273,50 @@ linkcheck_retries = 3
 # https://www.sphinx-doc.org/en/master/usage/extensions/index.html
 
 # NOTE: The canonical_sphinx extension is required for the starter pack.
-#       It automatically enables the following extensions:
-#       - custom-rst-roles
-#       - myst_parser
-#       - notfound.extension
-#       - related-links
-#       - sphinx_copybutton
-#       - sphinx_design
-#       - sphinx_reredirects
-#       - sphinx_tabs.tabs
-#       - sphinxcontrib.jquery
-#       - sphinxext.opengraph
-#       - terminal-output
-#       - youtube-links
 
 extensions = [
     "canonical_sphinx",
+    "notfound.extension",
+    "sphinx_design",
+    "sphinx_rerediraffe",
+    "sphinx_reredirects",
+    "sphinx_tabs.tabs",
+    "sphinxcontrib.jquery",
+    "sphinxext.opengraph",
+    "sphinx_config_options",
+    "sphinx_contributor_listing",
+    "sphinx_filtered_toctree",
+    "sphinx_related_links",
+    "sphinx_roles",
+    "sphinx_terminal",
+    "sphinx_ubuntu_images",
+    "sphinx_youtube_links",
     "sphinxcontrib.cairosvgconverter",
     "sphinx_last_updated_by_git",
     "sphinx.ext.intersphinx",
     "sphinx_sitemap",
 ]
 
+new_tab_link_show_external_link_icon = True
+
 # Excludes files or directories from processing
 
 exclude_patterns = [
-    "doc-cheat-sheet*",
+    ".venv*",
 ]
 
 # Adds custom CSS files, located under 'html_static_path'
 
-# html_css_files = []
+html_css_files = [
+    "https://assets.ubuntu.com/v1/d86746ef-cookie_banner.css",
+]
 
 
 # Adds custom JavaScript files, located under 'html_static_path'
 
-# html_js_files = []
+html_js_files = [
+    "https://assets.ubuntu.com/v1/287a5e8f-bundle.js",
+]
 
 
 # Specifies a reST snippet to be appended to each .rst file
@@ -348,8 +363,8 @@ rst_prolog = """
 
 # Workaround for https://github.com/canonical/canonical-sphinx/issues/34
 
-# if "discourse_prefix" not in html_context and "discourse" in html_context:
-#     html_context["discourse_prefix"] = html_context["discourse"] + "/t/"
+if "discourse_prefix" not in html_context and "discourse" in html_context:
+    html_context["discourse_prefix"] = f"{html_context['discourse']}/t/"
 
 # Workaround for substitutions.yaml
 
@@ -358,5 +373,7 @@ if os.path.exists('./reuse/substitutions.yaml'):
         myst_substitutions = yaml.safe_load(fd.read())
 
 # Add configuration for intersphinx mapping
-
-# intersphinx_mapping = {}
+# Map only the Sphinx documentation sets that you need to link to from your docs set.
+# intersphinx_mapping = {
+#     'sphinxcontrib-mermaid': ('https://sphinxcontrib-mermaid-demo.readthedocs.io/en/latest', None)
+# }
