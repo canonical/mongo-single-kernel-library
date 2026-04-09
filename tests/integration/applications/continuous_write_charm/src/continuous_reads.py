29be59e35c3b170b468220730273a0b3a9ea760d
Neha Oudin <neha@oudin.red>
2026-04-09 10:53:59 +0200

    wip

---
+++ tests/integration/applications/continuous_write_charm/src/continuous_reads.py
@@ -0,0 +1,76 @@
+# Copyright 2026 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""This file is meant to run in the background continuously reading data from MongoDB."""
+
+import json
+import random
+import signal
+import sys
+
+import math
+from pymongo import MongoClient
+from pymongo.errors import PyMongoError
+from pymongo.write_concern import WriteConcern
+
+DEFAULT_DB_NAME = "continuous_writes_database"
+DEFAULT_COLL_NAME = "continuous_writes_collection"
+
+run = True
+
+
+def sigterm_handler(_signo, _stack_frame):
+    global run
+    run = False
+
+def n_read_filename(db_name: str, coll_name: str) -> str:
+    return f"n_read_value-{db_name}-{coll_name}"
+
+
+def continous_reads(
+    connection_string: str,
+    db_name: str,
+    coll_name: str,
+):
+    failed_reads = []
+    reads = 0
+    while run:
+        client = MongoClient(
+            connection_string,
+            socketTimeoutMS=5000,
+        )
+        db = client[db_name]
+        test_collection = db[coll_name]
+        try:
+            if (rand:= random.random()) < 0.3:
+                # run some basic sampling
+                test_collection.aggregate([{"$sample": {"size": 10}}, {"$sort": {"number": 1}}])
+            elif rand < 0.6:
+                n_docs = test_collection.count_documents()
+                # get one single sample
+                test_collection.aggregate([{"$skip": math.floor(n_docs * random.random())}, {"$limit": 1}])
+            else:
+                n_docs = test_collection.count_documents()
+                test_collection.find({"number": {"$lte": math.floor(n_docs /2)}})
+        except PyMongoError as err:
+            failed_reads.append(str(err))
+            continue
+        finally:
+            client.close()
+
+        reads += 1
+
+    with open(n_read_filename(db_name, coll_name), "w") as fd:
+        json.dump({"reads": reads, "failed_reads": failed_reads}, fd)
+
+
+def main():
+    connection_string = sys.argv[1]
+    db_name = DEFAULT_DB_NAME if len(sys.argv) < 3 else sys.argv[2]
+    coll_name = DEFAULT_COLL_NAME if len(sys.argv) < 4 else sys.argv[3]
+    continous_reads(connection_string, db_name, coll_name)
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGTERM, sigterm_handler)
+    main()
---
+++ tests/integration/mongodb/release/test_release.py
@@ -0,0 +1,310 @@
+#!/usr/bin/env python3
+# Copyright 2026 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import asyncio
+from logging import getLogger
+
+import pytest
+from pytest_operator.plugin import OpsTest
+from tenacity import RetryError, Retrying
+from tenacity.stop import stop_after_delay
+from tenacity.wait import wait_fixed
+
+from tests.integration.helpers.backups import S3_APP_NAME, CloudConfigs, count_logical_backups
+from tests.integration.helpers.common import (
+    CONTINUOUS_WRITE_APPLICATION,
+    CONTINUOUS_WRITE_APPLICATION_BIS,
+    DATA_INTEGRATOR_APP_NAME,
+    DEFAULT_COLLECTION_NAME,
+    DEFAULT_DATABASE_NAME,
+    DEPLOYMENT_TIMEOUT,
+    READER_APPLICATION,
+    TIMEOUT,
+    UNIT_IDS,
+    count_writes,
+    deploy_application,
+    deploy_charm,
+    execute_on_mongod,
+    find_unit,
+    get_app_name,
+    relate_mongodb_and_application,
+    start_continous_writes,
+    start_continuous_reads,
+    stop_continous_writes,
+    stop_continuous_reads,
+)
+from tests.integration.helpers.ldap import (
+    LDAP_CERT_OFFER,
+    LDAP_OFFER,
+    apply_ldif,
+    consume_glauth_offers,
+    create_mongodb_user_roles,
+    deploy_glauth,
+    generate_mongodb_ldap_client,
+)
+from tests.integration.helpers.tls import (
+    TLS_CERTIFICATES_APP_NAME,
+    TLS_CERTIFICATES_BASE,
+    TLS_CERTIFICATES_CHANNEL,
+    integrate_apps_with_tls,
+)
+from tests.integration.helpers.types import Substrate
+
+logger = getLogger(__name__)
+
+SECOND_DB_NAME = f"{DEFAULT_DATABASE_NAME}_bis"
+SECOND_COLL_NAME = f"{DEFAULT_COLLECTION_NAME}_bis"
+
+
+@pytest.mark.abort_on_fail
+async def test_build_and_deploy(
+    ops_test: OpsTest,
+    mongodb_charm_name: str,
+    application_path: str,
+    substrate: Substrate,
+    mongodb_revision: int,
+    base_app_name: str,
+    kubernetes_model: str,
+):
+    """Build and deploy one unit of MongoDB."""
+    tls_config = {"ca-common-name": "MongoDB release CA"}
+    # it is possible for users to provide their own cluster for testing. Hence check if there
+    # is a pre-existing cluster.
+    asyncio.gather(
+        deploy_charm(
+            ops_test=ops_test,
+            revision=mongodb_revision,
+            charm=mongodb_charm_name,
+            substrate=substrate,
+            app_name=base_app_name,
+            num_units=len(UNIT_IDS),
+        ),
+        deploy_application(
+            ops_test, application_path=application_path, app_name=CONTINUOUS_WRITE_APPLICATION
+        ),
+        ops_test.model.deploy(
+            TLS_CERTIFICATES_APP_NAME,
+            channel=TLS_CERTIFICATES_CHANNEL,
+            config=tls_config,
+            base=TLS_CERTIFICATES_BASE,
+        ),
+        ops_test.model.deploy(
+            DATA_INTEGRATOR_APP_NAME,
+            channel="latest/stable",
+            series="noble",
+            config={"database-name": "test-database"},
+        ),
+        deploy_glauth(ops_test, kubernetes_model),
+    )
+
+    # Consume the offers exposed by glauth
+    await consume_glauth_offers(ops_test, kubernetes_model)
+
+    # Apply the LDIF file on glauth-utils to create users and groups
+    await apply_ldif(ops_test, kubernetes_model, "ldap_entries.ldif")
+
+    await ops_test.model.wait_for_idle(
+        apps=[base_app_name, TLS_CERTIFICATES_APP_NAME], timeout=DEPLOYMENT_TIMEOUT, status="active"
+    )
+
+
+@pytest.mark.abort_on_fail
+async def test_integrate_with_tls(
+    ops_test: OpsTest,
+):
+    """Tests that we can integrate with TLS without losing data."""
+    app_name = await get_app_name(ops_test)
+    await integrate_apps_with_tls(ops_test, applications=[app_name])
+
+    await ops_test.model.wait_for_idle(
+        apps=[app_name, TLS_CERTIFICATES_APP_NAME], status="active", timeout=1000, idle_period=60
+    )
+
+    await relate_mongodb_and_application(ops_test, app_name, CONTINUOUS_WRITE_APPLICATION)
+    await start_continous_writes(ops_test, CONTINUOUS_WRITE_APPLICATION)
+
+
+async def test_integrate_with_ldap(ops_test: OpsTest, substrate: Substrate):
+    app_name = await get_app_name(ops_test)
+
+    await ops_test.model.integrate(f"{LDAP_OFFER}:ldap", f"{app_name}:ldap")
+    await ops_test.model.integrate(
+        f"{LDAP_CERT_OFFER}:send-ca-cert", f"{app_name}:ldap-certificate-transfer"
+    )
+    # Create the roles on MongoDB
+    await create_mongodb_user_roles(
+        ops_test,
+        substrate,
+        app_name,
+        role_name="ou=superheroes,ou=users,dc=glauth,dc=com",
+        db=DEFAULT_DATABASE_NAME,
+    )
+
+    await ops_test.model.wait_for_idle(apps=[app_name], status="active", timeout=TIMEOUT)
+
+
+@pytest.mark.abort_on_fail
+async def test_integrate_second_client(ops_test: OpsTest, application_path: str):
+    app_name = await get_app_name(ops_test)
+
+    await deploy_application(
+        ops_test,
+        application_path=application_path,
+        app_name=CONTINUOUS_WRITE_APPLICATION_BIS,
+        database_name=SECOND_DB_NAME,
+    )
+    await relate_mongodb_and_application(ops_test, app_name, CONTINUOUS_WRITE_APPLICATION_BIS)
+    await start_continous_writes(
+        ops_test,
+        CONTINUOUS_WRITE_APPLICATION_BIS,
+        db_name=SECOND_DB_NAME,
+        coll_name=SECOND_COLL_NAME,
+    )
+
+
+@pytest.mark.abort_on_fail
+async def test_integrate_third_client(ops_test: OpsTest, application_path: str):
+    app_name = await get_app_name(ops_test)
+
+    await deploy_application(
+        ops_test,
+        application_path=application_path,
+        app_name=READER_APPLICATION,
+        database_name=DEFAULT_DATABASE_NAME,
+    )
+    await relate_mongodb_and_application(ops_test, app_name, READER_APPLICATION)
+
+    await start_continuous_reads(
+        ops_test,
+        READER_APPLICATION,
+        db_name=DEFAULT_DATABASE_NAME,
+        coll_name=DEFAULT_COLLECTION_NAME,
+    )
+
+
+@pytest.mark.abort_on_fail
+async def test_integrate_with_s3(ops_test: OpsTest, cloud_configs: CloudConfigs):
+    app_name = await get_app_name(ops_test)
+
+    # deploy the s3 integrator charm
+    await ops_test.model.deploy(S3_APP_NAME, channel="1/edge")
+    await ops_test.model.wait_for_idle(apps=[S3_APP_NAME], timeout=DEPLOYMENT_TIMEOUT)
+
+    configuration_parameters, credentials = cloud_configs["AWS"]
+    s3_integrator_unit = ops_test.model.applications[S3_APP_NAME].units[0]
+
+    # apply new configuration options
+    await ops_test.model.applications[S3_APP_NAME].set_config(configuration_parameters)
+    action = await s3_integrator_unit.run_action(action_name="sync-s3-credentials", **credentials)
+    await action.wait()
+
+    await ops_test.model.wait_for_idle(
+        apps=[S3_APP_NAME, app_name], status="active", timeout=TIMEOUT
+    )
+
+    leader_unit = await find_unit(ops_test, leader=True, app_name=app_name)
+    action = await leader_unit.run_action(action_name="create-backup")
+    backup_result = await action.wait()
+
+    logger.info(f"Create backup result {backup_result.results=}")
+    assert "backup started" in backup_result.results["backup-status"], "backup didn't start"
+    try:
+        for attempt in Retrying(stop=stop_after_delay(60), wait=wait_fixed(5)):
+            with attempt:
+                backups = await count_logical_backups(leader_unit)
+                assert backups == 1
+    except RetryError:
+        assert backups == 1, "Backup not created."
+
+
+@pytest.mark.abort_on_fail
+async def tests_restore_backup(ops_test: OpsTest, substrate: Substrate):
+    app_name = await get_app_name(ops_test)
+
+    first_reported_writes = await stop_continous_writes(ops_test, CONTINUOUS_WRITE_APPLICATION)
+    second_reported_writes = await stop_continous_writes(
+        ops_test,
+        CONTINUOUS_WRITE_APPLICATION_BIS,
+        db_name=SECOND_DB_NAME,
+        coll_name=SECOND_COLL_NAME,
+    )
+    leader_unit = await find_unit(ops_test, leader=True, app_name=app_name)
+    # count total writes
+    first_number_writes = await count_writes(ops_test, substrate, app_name, leader_unit)
+    second_number_writes = await count_writes(
+        ops_test,
+        substrate,
+        app_name,
+        leader_unit,
+        db_name=SECOND_DB_NAME,
+        coll_name=SECOND_COLL_NAME,
+    )
+    assert first_number_writes == first_reported_writes
+    assert second_number_writes == second_reported_writes
+
+    # find most recent backup id and restore
+    action = await leader_unit.run_action(action_name="list-backups")
+    list_result = await action.wait()
+    list_result = list_result.results["backups"]
+    most_recent_backup = list_result.split("\n")[-1]
+
+    backup_id = most_recent_backup.split()[0]
+
+    action = await leader_unit.run_action(action_name="restore", **{"backup-id": backup_id})
+    restore = await action.wait()
+    logger.info(f"Restore backup result {restore.results=}")
+    assert restore.results["restore-status"] == "restore started", "restore not successful"
+
+    with ops_test.fast_forward("60s"):
+        (await ops_test.model.wait_for_idle(apps=[app_name], status="active", idle_period=15),)
+
+
+@pytest.mark.abort_on_fail
+async def test_ldap_user_can_write(ops_test: OpsTest, substrate: Substrate):
+    """Checks that the LDAP user can write to the DB.
+
+    This checks both authentication and authorisation.
+    """
+    app_name = await get_app_name(ops_test)
+
+    # We create a client which should be able to write
+    uri = await generate_mongodb_ldap_client(
+        ops_test,
+        substrate,
+        app_name,
+        database="superdb",
+        username="cn=johndoe,ou=superheroes,ou=users,dc=glauth,dc=com",
+        password="dogood",
+    )
+
+    result = await execute_on_mongod(
+        ops_test, app_name, substrate, uri, "db.test.insertOne({number: 1})"
+    )
+    assert result.succeeded, "Failed to insert value with LDAP client"
+
+    result = await execute_on_mongod(
+        ops_test, app_name, substrate, uri, "db.test.findOne({number: 1})"
+    )
+    assert result.succeeded, "Failed to read value with LDAP client"
+
+    result = await execute_on_mongod(
+        ops_test,
+        app_name,
+        substrate,
+        uri,
+        f"db.{DEFAULT_COLLECTION_NAME}.find().limit(10)",
+    )
+    assert result.succeeded, "Failed to read value with LDAP client"
+
+
+@pytest.mark.abort_on_fail
+async def test_valid_reads(ops_test: OpsTest):
+    reads, failed_reads = await stop_continuous_reads(
+        ops_test,
+        READER_APPLICATION,
+        db_name=DEFAULT_DATABASE_NAME,
+        coll_name=DEFAULT_COLLECTION_NAME,
+    )
+    assert reads > 1000
+    assert len(failed_reads) == 0
