7d5a24041ccd9b6fc8839bc1ec2fc942707e0da4
Neha Oudin <neha@oudin.red>
2026-04-09 09:43:31 +0200

    patch(DPE-9750): allowConnectionsWithoutCertificates

--- tests/integration/mongos/test_tls_race_condition.py
+++ tests/integration/mongos/test_tls_race_condition.py
@@ -29,8 +29,8 @@ async def test_build_and_deploy(
     mongodb_charm: str,
     mongos_charm: str,
     mongod_resource: dict,
-    mongos_resource: dict,
-    mongos_client_application_path: str,
+    mongos_resource: dict[str, str],
+    application_path: str,
 ) -> None:
     """Build and deploy a sharded cluster."""
     await deploy_cluster_components(
@@ -40,11 +40,15 @@ async def test_build_and_deploy(
         mongos_charm,
         mongod_resource,
         mongos_resource,
-        mongos_client_application_path,
+        application_path,
     )
     await build_cluster(ops_test, substrate, integrate_with_mongos=False)
+    config = {"ca-common-name": "Test CA"}
     await ops_test.model.deploy(
-        TLS_CERTIFICATES_APP_NAME, channel=TLS_CERTIFICATES_CHANNEL, base=TLS_CERTIFICATES_BASE
+        TLS_CERTIFICATES_APP_NAME,
+        channel=TLS_CERTIFICATES_CHANNEL,
+        base=TLS_CERTIFICATES_BASE,
+        config=config,
     )
     await ops_test.model.wait_for_idle(
         apps=[TLS_CERTIFICATES_APP_NAME],
