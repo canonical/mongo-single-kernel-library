9f3400e8a2d39de719e37ec7e3f893c3b95f8006
Neha Oudin <neha@oudin.red>
2026-04-20 15:50:09 +0200

    fix: correct series

--- tests/integration/mongodb/relations/test_sharding_relations.py
+++ tests/integration/mongodb/relations/test_sharding_relations.py
@@ -106,7 +106,7 @@ async def test_build_and_deploy(
         client_relation_charm_path,
         application_name=APPLICATION_APP_NAME,
         num_units=1,
-        series="noble",
+        series="jammy",
     )
 
     await ops_test.model.wait_for_idle(
