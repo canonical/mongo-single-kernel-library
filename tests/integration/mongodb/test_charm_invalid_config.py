8f439bdccd52c2075caf61c96e7813da56ff4baf
Neha Oudin <neha@oudin.red>
2026-09-18 17:00:26 +0200

    patch(DPE-11175): test_charm_invalid_config.py

--- tests/integration/mongodb/test_charm_invalid_config.py
+++ tests/integration/mongodb/test_charm_invalid_config.py
@@ -3,20 +3,28 @@
 # See LICENSE file for licensing details.
 import logging
 
+import jubilant
 import pytest
 from pymongo import MongoClient
-from pytest_operator.plugin import OpsTest
 
-from tests.integration.helpers.common import (
+from single_kernel_mongo.config.statuses import MongoDBStatuses
+from tests.integration.helpers.constants import (
+    CHARMED_OPERATOR_USERNAME,
     DEPLOYMENT_TIMEOUT,
+    TIMEOUT,
     UNIT_IDS,
-    check_app_status,
+)
+from tests.integration.helpers.jubilant_common import (
     deploy_charm,
-    find_unit,
-    get_address_of_unit,
-    get_app_name,
+    find_leader,
+    get_ip_from_unit,
     get_password,
     unit_uri,
+)
+from tests.integration.helpers.status_helpers import (
+    are_agents_idle,
+    are_apps_active_and_agents_idle,
+    does_status_match,
 )
 from tests.integration.helpers.types import Substrate
 
@@ -24,43 +32,68 @@ logger = logging.getLogger(__name__)
 
 
 @pytest.mark.abort_on_fail
-async def test_build_and_deploy(
-    ops_test: OpsTest, mongodb_charm: str, substrate: Substrate, mongod_resource, base_app_name
-):
+def test_build_and_deploy(
+    juju: jubilant.Juju,
+    substrate: Substrate,
+    mongodb_charm: str,
+    mongod_resource: dict[str, str],
+    base_app_name: str,
+) -> None:
     """Deploys the charm with an invalid config.
 
     Then waits for the status to display, change it to a correct value and
     checks that the service starts afterwards.
     """
-    await deploy_charm(
-        ops_test=ops_test,
+    app_name = base_app_name
+
+    deploy_charm(
+        juju=juju,
         charm=mongodb_charm,
         substrate=substrate,
         mongod_resource=mongod_resource,
-        app_name=base_app_name,
+        app_name=app_name,
         num_units=len(UNIT_IDS),
         config={"role": "invalidrole"},
     )
-    await ops_test.model.wait_for_idle(
-        apps=[base_app_name], raise_on_blocked=False, timeout=DEPLOYMENT_TIMEOUT
+    juju.wait(
+        lambda status: are_agents_idle(status, app_name, idle_period=30, unit_count=len(UNIT_IDS)),
+        timeout=DEPLOYMENT_TIMEOUT,
+        delay=5,
+        successes=3,
     )
 
-    # Check that status is blocked.
-    await check_app_status(ops_test, base_app_name, "blocked", "The role config option is invalid")
+    does_status_match(
+        juju.status(),
+        expected_unit_statuses=None,
+        expected_app_statuses={app_name: [MongoDBStatuses.INVALID_ROLE.value]},
+        num_units={app_name: len(UNIT_IDS)},
+    )
 
+    juju.config(app_name, {"role": "replication"})
     # Check that we can resolve it
-    await ops_test.model.applications[base_app_name].set_config({"role": "replication"})
-    await ops_test.model.wait_for_idle(
-        apps=[base_app_name], raise_on_blocked=False, status="active"
-    )
-    app_name = await get_app_name(ops_test)
-    leader_unit = await find_unit(ops_test, leader=True, app_name=app_name)
-    password = await get_password(ops_test, app_name=app_name)
-    ip_address = await get_address_of_unit(
-        ops_test, substrate, int(leader_unit.name.split("/")[1]), app_name
+    juju.wait(
+        lambda status: are_apps_active_and_agents_idle(
+            status, app_name, idle_period=30, unit_count=len(UNIT_IDS)
+        ),
+        timeout=TIMEOUT,
+        delay=5,
+        successes=3,
     )
 
-    client = MongoClient(unit_uri(ip_address, password, app_name), directConnection=True)
+    _, leader_unit_info = find_leader(juju=juju, app_name=app_name)
 
-    # Good, we have a valid version, the server is running.
+    password = get_password(juju, app_name=app_name, username=CHARMED_OPERATOR_USERNAME)
+
+    ip_address = get_ip_from_unit(substrate=substrate, unit_info=leader_unit_info)
+
+    client = MongoClient(
+        unit_uri(
+            username=CHARMED_OPERATOR_USERNAME,
+            ip_address=ip_address,
+            password=password,
+            replica_set=app_name,
+        ),
+        directConnection=True,
+    )
+
     assert client.server_info()["version"].split("-")[0] is not None
