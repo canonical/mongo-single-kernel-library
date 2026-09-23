a91a5ddf02a707531d2ef7518ae4fe7576534854
Neha Oudin <neha@oudin.red>
2026-09-23 14:59:28 +0200

    fix: helper

--- tests/integration/helpers/jubilant_tls.py
+++ tests/integration/helpers/jubilant_tls.py
@@ -120,7 +120,7 @@ def check_tls(
         model_name = juju.model
         host = f"mongodb-k8s-{unit_id}.mongodb-k8s-endpoints.{model_name}.svc.cluster.local"
 
-    uri = unit_uri(username, password, ip_address=host, mongos=True)
+    uri = unit_uri(username, password, ip_address=host, mongos=mongos)
 
     try:
         for attempt in Retrying(
