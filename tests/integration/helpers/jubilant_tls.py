05e4176fb0377ab3d91d16d2cdbdcddf66aec852
Neha Oudin <neha@oudin.red>
2026-09-24 10:53:27 +0200

    fix: DNS

--- tests/integration/helpers/jubilant_tls.py
+++ tests/integration/helpers/jubilant_tls.py
@@ -118,7 +118,7 @@ def check_tls(
     if substrate == "microk8s":
         unit_id = get_unit_id(unit_name)
         model_name = juju.model
-        host = f"mongodb-k8s-{unit_id}.mongodb-k8s-endpoints.{model_name}.svc.cluster.local"
+        host = f"{app_name}-{unit_id}.{app_name}-endpoints.{model_name}.svc.cluster.local"
 
     uri = unit_uri(username, password, ip_address=host, mongos=mongos)
 
